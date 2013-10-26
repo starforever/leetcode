85226cd722f02a0e6122f1de6d552888123279c1
starforever <starforever00@gmail.com>
2013-10-27 00:23:21 +0800

    (Candy) [Accepted]

--- Candy/Solution.java
+++ Candy/Solution.java
@@ -1,29 +1,49 @@
 public class Solution
 {
+
+  static class Child implements Comparable<Child>
+  {
+    int rating;
+    int pos;
+
+    public Child (int rating, int pos)
+    {
+      this.rating = rating;
+      this.pos = pos;
+    }
+
+    public int compareTo (Child o)
+    {
+      if (rating < o.rating)
+        return -1;
+      else if (rating > o.rating)
+        return 1;
+      else
+        return 0;
+    }
+  }
+
   public int candy (int[] ratings)
   {
-    int cnt = 1;
-    int lastUp = 1, lastDown = 1;
-    for (int i = 1; i < ratings.length; ++i)
+    int n = ratings.length;
+    Child[] children = new Child[n];
+    for (int i = 0; i < n; ++i)
+      children[i] = new Child(ratings[i], i);
+    Arrays.sort(children);
+    int[] candy = new int[n];
+    for (int i = 0; i < n; ++i)
     {
-      if (ratings[i] > ratings[i - 1])
-      {
-        lastDown = 0;
-        cnt += lastUp + 1;
-        ++lastUp;
-      }
-      else if (ratings[i] < ratings[i - 1])
-      {
-        lastUp = 1;
-        cnt += lastDown + 1;
-        ++lastDown;
-      }
-      else
-      {
-        ++cnt;
-        lastUp = lastDown = 1;
-      }
+      int p = children[i].pos;
+      int c = 0;
+      if (p - 1 >= 0 && ratings[p - 1] < ratings[p])
+        c = Math.max(c, candy[p - 1]);
+      if (p + 1 < n && ratings[p + 1] < ratings[p])
+        c = Math.max(c, candy[p + 1]);
+      candy[p] = c + 1;
     }
-    return cnt;
+    int sum = 0;
+    for (int i = 0; i < n; ++i)
+      sum += candy[i];
+    return sum;
   }
 }
