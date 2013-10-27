80f43b1153129f321302517d5b2961cae598dd63
starforever <starforever00@gmail.com>
2013-10-28 00:33:08 +0800

    More arguments for commit.sh

--- commit.sh
+++ commit.sh
@@ -1,2 +1,13 @@
 git add "$1"/Solution.java
-git commit -m "($1) [$2]"
+if [ "$2" ]
+then
+  status="$2"
+else
+  status="Accepted"
+fi
+if [ "$3" ]
+then
+  git commit -m "($1) [$status] $3"
+else
+  git commit -m "($1) [$status]"
+fi
