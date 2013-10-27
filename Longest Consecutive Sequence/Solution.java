2425feb9e8664863078045f8d04cdc032004ad63
starforever <starforever00@gmail.com>
2013-10-27 21:40:24 +0800

    (Longest Consecutive Sequence) [Accepted]

--- Longest Consecutive Sequence/Solution.java
+++ Longest Consecutive Sequence/Solution.java
@@ -1,37 +1,41 @@
 public class Solution
 {
 
-  static class State
+  HashMap<Integer, Boolean> label;
+
+  int findSeq (int x)
   {
-    int numLeft = 0, numRight = 0;
+    label.put(x, true);
+    int cnt = 1;
+    int y = x + 1;
+    while (label.containsKey(y))
+    {
+      label.put(y, true);
+      ++cnt;
+      ++y;
+    }
+    y = x - 1;
+    while (label.containsKey(y))
+    {
+      label.put(y, true);
+      ++cnt;
+      --y;
+    }
+    return cnt;
   }
 
   public int longestConsecutive (int[] num)
   {
-    if (num == null)
+    if (num == null || num.length == 0)
       return 0;
-    HashMap<Integer, State> stateTable = new HashMap<Integer, State>();
+    int N = num.length;
+    label = new HashMap<Integer, Boolean>();
+    for (int i = 0; i < N; ++i)
+      label.put(num[i], false);
     int maxSeq = 0;
-    for (int i = 0; i < num.length; ++i)
-    {
-      State sL = null, sR = null, s = new State();
-      if (stateTable.containsKey(num[i] - 1))
-      {
-        sL = stateTable.get(num[i] - 1);
-        s.numLeft = sL.numLeft + 1;
-      }
-      if (stateTable.containsKey(num[i] + 1))
-      {
-        sR = stateTable.get(num[i] + 1);
-        s.numRight = sR.numRight + 1;
-      }
-      stateTable.put(num[i], s);
-      if (sL != null)
-        sL.numRight = s.numRight + 1;
-      if (sR != null)
-        sR.numLeft = s.numLeft + 1;
-      maxSeq = Math.max(maxSeq, s.numLeft + s.numRight + 1);
-    }
+    for (int i = 0; i < N; ++i)
+      if (!label.get(num[i]))
+        maxSeq = Math.max(maxSeq, findSeq(num[i]));
     return maxSeq;
   }
 
