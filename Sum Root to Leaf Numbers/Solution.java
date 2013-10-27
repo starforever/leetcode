f7d3f768560302350325887ec951137fdb2e81a9
starforever <starforever00@gmail.com>
2013-10-27 18:26:46 +0800

    (Sum Root to Leaf Numbers) [Accepted]

---
+++ Sum Root to Leaf Numbers/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution
+{
+
+  int sum;
+
+  void DFS (TreeNode p, int num)
+  {
+    num = num * 10 + p.val;
+    if (p.left == null && p.right == null)
+      sum += num;
+    if (p.left != null)
+      DFS(p.left, num);
+    if (p.right != null)
+      DFS(p.right, num);
+  }
+
+  public int sumNumbers (TreeNode root)
+  {
+    if (root == null)
+      return 0;
+    sum = 0;
+    DFS(root, 0);
+    return sum;
+  }
+
+}
