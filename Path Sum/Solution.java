f1622bdb9190157ec0a3b842297d621e1e03fc5d
starforever <starforever00@gmail.com>
2013-10-28 21:18:13 +0800

    (Path Sum) [Accepted]

---
+++ Path Sum/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution
+{
+
+  public boolean hasPathSum (TreeNode root, int sum)
+  {
+    if (root == null)
+      return false;
+    if (root.left == null && root.right == null)
+      return root.val == sum;
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+  }
+
+}
