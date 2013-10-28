ce924eb48b7482373af3e356d04344847c517873
starforever <starforever00@gmail.com>
2013-10-28 16:42:58 +0800

    Improved init.sh with existence checking and pretty output

--- init.sh
+++ init.sh
@@ -1,2 +1,13 @@
-mkdir "$1"
-touch "$1"/Solution.java
+if [ "$1" ]
+then
+  if [ -d "$1" ]
+  then
+    echo "Directory $1 already exists."
+  else
+    mkdir "$1"
+    touch "$1"/Solution.java
+    echo "Initialized $1."
+  fi
+else
+  echo "Problem name required."
+fi
