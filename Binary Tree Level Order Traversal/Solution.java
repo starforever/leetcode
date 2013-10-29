4352b193b079179137c4fc50daf05b50c6410f15
starforever <starforever00@gmail.com>
2013-10-29 21:52:52 +0800

    (Binary Tree Level Order Traversal) [Accepted] Much simpler implementation using DFS

--- Binary Tree Level Order Traversal/Solution.java
+++ Binary Tree Level Order Traversal/Solution.java
@@ -10,30 +10,25 @@
 
 public class Solution
 {
+
+  ArrayList<ArrayList<Integer>> levelList;
+
+  void DFS (TreeNode p, int level)
+  {
+    if (p == null)
+      return;
+    if (level == levelList.size())
+      levelList.add(new ArrayList<Integer>());
+    levelList.get(level).add(p.val);
+    DFS(p.left, level + 1);
+    DFS(p.right, level + 1);
+  }
+
   public ArrayList<ArrayList<Integer>> levelOrder (TreeNode root)
   {
-    ArrayList<ArrayList<Integer>> levelList = new ArrayList<ArrayList<Integer>>();
-    if (root == null)
-      return levelList;
-    LinkedList<TreeNode> curLevel = new LinkedList<TreeNode>();
-    curLevel.add(root);
-    while (!curLevel.isEmpty())
-    {
-      ArrayList<Integer> row = new ArrayList<Integer>();
-      for (TreeNode p : curLevel)
-        row.add(p.val);
-      levelList.add(row);
-      LinkedList<TreeNode> nextLevel = new LinkedList<TreeNode>();
-      while (!curLevel.isEmpty())
-      {
-        TreeNode p = curLevel.remove();
-        if (p.left != null)
-          nextLevel.add(p.left);
-        if (p.right != null)
-          nextLevel.add(p.right);
-      }
-      curLevel = nextLevel;
-    }
+    levelList = new ArrayList<ArrayList<Integer>>();
+    DFS(root, 0);
     return levelList;
   }
+
 }
