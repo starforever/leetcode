ff498d2d4e1242d0fe77bf74c0015a8de56be90b
starforever <starforever00@gmail.com>
2013-11-01 01:37:18 +0800

    (Binary Tree Inorder Traversal) [Accepted]

---
+++ Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution
+{
+
+  static class State
+  {
+    int stage = 0;
+    TreeNode cur;
+
+    public State (TreeNode cur)
+    {
+      this.cur = cur;
+    }
+  }
+
+  public ArrayList<Integer> inorderTraversal (TreeNode root)
+  {
+    ArrayList<Integer> ret = new ArrayList<Integer>();
+    if (root == null)
+      return ret;
+    LinkedList<State> stack = new LinkedList<State>();
+    stack.add(new State(root));
+    while (!stack.isEmpty())
+    {
+      State s = stack.getLast();
+      switch (s.stage)
+      {
+        case 0:
+          if (s.cur.left != null)
+            stack.addLast(new State(s.cur.left));
+          ++s.stage;
+          break;
+        case 1:
+          ret.add(s.cur.val);
+          if (s.cur.right != null)
+            stack.addLast(new State(s.cur.right));
+          ++s.stage;
+          break;
+        case 2:
+          stack.removeLast();
+          break;
+      }
+    }
+    return ret;
+  }
+}
