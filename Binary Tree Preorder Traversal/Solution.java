031539eaa651fb8a5e2b1aa2e30df6b3795ad905
starforever <starforever00@gmail.com>
2013-11-08 21:02:45 +0800

    (Binary Tree Preorder Traversal) [Accepted]

---
+++ Binary Tree Preorder Traversal/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *   int val;
+ *   TreeNode left;
+ *   TreeNode right;
+ *   TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution
+{
+  public ArrayList<Integer> preorderTraversal (TreeNode root)
+  {
+    ArrayList<Integer> ret = new ArrayList<Integer>();
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    if (root != null)
+      stack.push(root);
+    while (!stack.empty())
+    {
+      TreeNode p = stack.pop();
+      ret.add(p.val);
+      if (p.right != null)
+        stack.push(p.right);
+      if (p.left != null)
+        stack.push(p.left);
+    }
+    return ret;
+  }
+}
