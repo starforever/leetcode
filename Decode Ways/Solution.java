e4e450262f6663000346c09ee484f1bfa3ca52a6
starforever <starforever00@gmail.com>
2013-11-10 23:50:46 +0800

    (Decode Ways) [Accepted] O(1) extra memory cost

--- Decode Ways/Solution.java
+++ Decode Ways/Solution.java
@@ -5,18 +5,18 @@ public class Solution
     int n = s.length();
     if (n == 0)
       return 0;
-    int[] num = new int[n + 1];
-    num[0] = 1;
-    num[1] = s.charAt(0) != '0' ? 1 : 0;
+    int last2 = 1, last1 = s.charAt(0) != '0' ? 1 : 0, cur = last1;
     for (int i = 2; i <= n; ++i)
     {
-      num[i] = 0;
+      cur = 0;
       char a = s.charAt(i - 2), b = s.charAt(i - 1);
       if (b != '0')
-        num[i] += num[i - 1];
+        cur += last1;
       if (a == '1' || a == '2' && b <= '6')
-        num[i] += num[i - 2];
+        cur += last2;
+      last2 = last1;
+      last1 = cur;
     }
-    return num[n];
+    return cur;
   }
 }
