bccbe324d5ce6c792dac8998fc844c8c22dec889
starforever <starforever00@gmail.com>
2013-11-12 22:12:00 +0800

    (Remove Duplicates from Sorted List) [Accepted] Removed use of HashSet

--- Remove Duplicates from Sorted List/Solution.java
+++ Remove Duplicates from Sorted List/Solution.java
@@ -4,22 +4,14 @@ public class Solution
   {
     if (head == null)
       return null;
-    HashSet<Integer> set = new HashSet<Integer>();
-    set.add(head.val);
     ListNode prev = head, cur = head.next;
     while (cur != null)
     {
-      if (set.contains(cur.val))
-      {
+      if (cur.val == prev.val)
         prev.next = cur.next;
-        cur = cur.next;
-      }
       else
-      {
-        set.add(cur.val);
         prev = cur;
-        cur = cur.next;
-      }
+      cur = cur.next;
     }
     return head;
   }
