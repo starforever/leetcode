80742edf6d92767988508cc970d65f7e6f16003c
starforever <starforever00@gmail.com>
2013-11-13 21:07:37 +0800

    (Rotate List) [Accepted]

--- Rotate List/Solution.java
+++ Rotate List/Solution.java
@@ -1,19 +1,31 @@
 public class Solution
 {
+
+  int length (ListNode head)
+  {
+    int n = 0;
+    while (head != null)
+    {
+      ++n;
+      head = head.next;
+    }
+    return n;
+  }
+
   public ListNode rotateRight (ListNode head, int k)
   {
+    if (head == null)
+      return null;
+    int n = length(head);
+    k %= n;
     if (k == 0)
       return head;
     ListNode p1 = head, p2 = head;
-    while (k > 0 && p2 != null)
+    while (k > 0)
     {
       p2 = p2.next;
       --k;
     }
-    if (k > 0)
-      return null;
-    if (p2 == null)
-      return head;
     while (p2.next != null)
     {
       p1 = p1.next;
@@ -24,4 +36,5 @@ public class Solution
     p1.next = null;
     return head;
   }
+
 }
