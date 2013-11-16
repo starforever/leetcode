bba3fd01d330916660b296c63a7c40604575e79b
starforever <starforever00@gmail.com>
2013-11-16 22:41:21 +0800

    (First Missing Positive) [Accepted] O(1) space

--- First Missing Positive/Solution.java
+++ First Missing Positive/Solution.java
@@ -5,12 +5,20 @@ public class Solution
     if (A == null)
       return 1;
     int N = A.length;
-    boolean[] occur = new boolean[N];
-    for (int i = 0; i < N; ++i)
-      if (A[i] >= 1 && A[i] <= N)
-        occur[A[i] - 1] = true;
-    for (int i = 0; i < N; ++i)
-      if (!occur[i])
+    int i = 0;
+    while (i < N)
+    {
+      if (A[i] >= 1 && A[i] <= N && A[i] != i + 1 && A[A[i] - 1] != A[i])
+      {
+        int tmp = A[A[i] - 1];
+        A[A[i] - 1] = A[i];
+        A[i] = tmp;
+      }
+      else
+        ++i;
+    }
+    for (i = 0; i < N; ++i)
+      if (A[i] != i + 1)
         return i + 1;
     return N + 1;
   }
