a8bcff9794b99809989f4dbf9d7e8a33f602a44c
starforever <starforever00@gmail.com>
2013-11-18 19:33:50 +0800

    (Reverse Integer) [Accepted] Overflow detection

--- Reverse Integer/Solution.java
+++ Reverse Integer/Solution.java
@@ -1,17 +1,27 @@
 public class Solution
 {
-  public int reverse (int x)
+
+  long reverse (long x)
   {
     if (x < 0)
       return -reverse(-x);
-    int y = 0;
+    long y = 0;
     while (x > 0)
     {
-      int d = x % 10;
+      long d = x % 10;
       x /= 10;
       y *= 10;
       y += d;
     }
     return y;
   }
+
+  public int reverse (int x)
+  {
+    long y = reverse((long)x);
+    if (y < Integer.MIN_VALUE || y > Integer.MAX_VALUE)
+      throw new RuntimeException();
+    return (int)y;
+  }
+
 }
