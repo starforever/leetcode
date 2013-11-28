05bfa51f294dca08e865d79ce4f84ec165cac667
starforever <starforever00@gmail.com>
2013-11-28 11:34:35 +0800

    (Evaluate Reverse Polish Notation) [Accepted]

---
+++ Evaluate Reverse Polish Notation/Solution.java
@@ -0,0 +1,62 @@
+public class Solution
+{
+
+  enum Operator {ADD, SUBTRACT, MULTIPLY, DIVIDE, UNKNOWN}
+
+  Operator getOp (String token)
+  {
+    if (token.length() != 1)
+      return Operator.UNKNOWN;
+    char c = token.charAt(0);
+    switch (c)
+    {
+      case '+':
+        return Operator.ADD;
+      case '-':
+        return Operator.SUBTRACT;
+      case '*':
+        return Operator.MULTIPLY;
+      case '/':
+        return Operator.DIVIDE;
+      default:
+        return Operator.UNKNOWN;
+    }
+  }
+
+  int eval (int a, int b, Operator op)
+  {
+    switch (op)
+    {
+      case ADD:
+        return a + b;
+      case SUBTRACT:
+        return a - b;
+      case MULTIPLY:
+        return a * b;
+      case DIVIDE:
+        return a / b;
+      default:
+        return -1;
+    }
+  }
+
+  public int evalRPN (String[] tokens)
+  {
+    Stack<Integer> stack = new Stack<Integer>();
+    for (int i = 0; i < tokens.length; ++i)
+    {
+      Operator op = getOp(tokens[i]);
+      if (op != Operator.UNKNOWN)
+      {
+        int b = stack.pop(), a = stack.pop();
+        stack.push(eval(a, b, op));
+      }
+      else
+      {
+        stack.push(Integer.parseInt(tokens[i]));
+      }
+    }
+    return stack.peek();
+  }
+
+}
