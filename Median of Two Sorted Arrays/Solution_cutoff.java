c66a14a52351df0eb04050a1b8c5e0ce50eda05f
starforever <starforever00@gmail.com>
2014-01-03 17:42:45 +0800

    (Median of Two Sorted Arrays) [Accepted] Code refinements

--- Median of Two Sorted Arrays/Solution_cutoff.java
+++ Median of Two Sorted Arrays/Solution_cutoff.java
@@ -3,33 +3,28 @@ public class Solution
 
   int findKth (int k, int[] A, int start1, int len1, int[] B, int start2, int len2)
   {
-    if (len1 > len2)
-      return findKth(k, B, start2, len2, A, start1, len1);
-    if (len1 == 0)
-      return B[start2 + k];
+    if (lenA > lenB)
+      return findKth(k, B, startB, lenB, A, startA, lenA);
+    if (lenA == 0)
+      return B[startB + k];
     if (k == 0)
-      return Math.min(A[start1], B[start2]);
-    int mid1 = Math.min((k + 1) / 2, len1);
-    int mid2 = k + 1 - mid1;
-    if (A[start1 + mid1 - 1] < B[start2 + mid2 - 1])
-      return findKth(k - mid1, A, start1 + mid1, len1 - mid1, B, start2, len2);
+      return Math.min(A[startA], B[startB]);
+    int kA = Math.min((k + 1) / 2, lenA), kB = k + 1 - kA;
+    if (A[startA + kA - 1] < B[startB + kB - 1])
+      return findKth(k - kA, A, startA + kA, lenA - kA, B, startB, lenB);
     else
-      return findKth(k - mid2, A, start1, len1, B, start2 + mid2, len2 - mid2);
+      return findKth(k - kB, A, startA, lenA, B, startB + kB, lenB - kB);
   }
 
   public double findMedianSortedArrays (int[] A, int[] B)
   {
-    if (A == null)
-      A = new int[0];
-    if (B == null)
-      B = new int[0];
-    int N = A.length, M = B.length;
-    if (N + M == 0)
+    int N = A.length + B.length;
+    if (N == 0)
       return 0.0;
-    if ((N + M) % 2 == 1)
-      return findKth((N + M) / 2, A, 0, N, B, 0, M);
+    if (N % 2 == 1)
+      return findKth(N / 2, A, 0, A.length, B, 0, B.length);
     else
-      return (findKth((N + M) / 2, A, 0, N, B, 0, M) + findKth((N + M) / 2 - 1, A, 0, N, B, 0, M)) * 0.5;
+      return (findKth(N / 2 - 1, A, 0, A.length, B, 0, B.length) + findKth(N / 2, A, 0, A.length, B, 0, B.length)) * 0.5;
   }
 
 }
