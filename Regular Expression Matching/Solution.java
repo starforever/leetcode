ba77bf007b1920f7df24e30381e93d1724305613
starforever <starforever00@gmail.com>
2014-01-05 17:15:11 +0800

    (Regular Expression Matching) [Accepted] Code refinements

--- Regular Expression Matching/Solution.java
+++ Regular Expression Matching/Solution.java
@@ -1,32 +1,36 @@
 public class Solution
 {
+
+  boolean isValid (String p)
+  {
+    if (p.length() > 0 && p.charAt(0) == '*')
+      return false;
+    for (int i = 1; i < p.length(); ++i)
+      if (p.charAt(i) == '*' && p.charAt(i - 1) == '*')
+        return false;
+    return true;
+  }
+
   public boolean isMatch (String s, String p)
   {
-    if (s == null || p == null)
-      return false;
+    if (!isValid(p))
+      throw new RuntimeException();
     int N = s.length(), M = p.length();
     boolean[][] match = new boolean[N + 1][M + 1];
     match[0][0] = true;
     for (int i = 1; i <= N; ++i)
       match[i][0] = false;
-    for (int i = 0; i <= N; ++i)
+    for (int j = 1; j <= M; ++j)
+      match[0][j] = p.charAt(j - 1) == '*' && match[0][j - 2];
+    for (int i = 1; i <= N; ++i)
       for (int j = 1; j <= M; ++j)
       {
-        match[i][j] = false;
-        if (p.charAt(j - 1) == '*')
-        {
-          if (j < 2 || p.charAt(j - 2) == '*')
-            return false;
-          match[i][j] |= match[i][j - 2];
-          if (i > 0 && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)))
-            match[i][j] |= match[i - 1][j];
-        }
+        if (p.charAt(j - 1) != '*')
+          match[i][j] = (p.charAt(j - 1) == '.' || s.charAt(i - 1) == p.charAt(j - 1)) && match[i - 1][j - 1];
         else
-        {
-          if (i > 0 && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)))
-            match[i][j] |= match[i - 1][j - 1];
-        }
+          match[i][j] = match[i][j - 2] || (p.charAt(j - 2) == '.' || s.charAt(i - 1) == p.charAt(j - 2)) && match[i - 1][j];
       }
     return match[N][M];
   }
+
 }
