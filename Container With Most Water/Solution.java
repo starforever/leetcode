1f560fc4203f3ee1ba102913e713d66de1d6c630
starforever <starforever00@gmail.com>
2014-01-05 18:35:59 +0800

    (Container With Most Water) [Accepted] Code refinements

--- Container With Most Water/Solution.java
+++ Container With Most Water/Solution.java
@@ -1,21 +1,18 @@
 public class Solution
 {
-
   public int maxArea (int[] height)
   {
-    if (height == null || height.length < 2)
-      return 0;
     int left = 0, right = height.length - 1;
     int maxArea = 0;
     while (right - left >= 1)
     {
-      maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
-      if (height[left] < height[right])
+      int low = Math.min(height[left], height[right]);
+      maxArea = Math.max(maxArea, (right - left) * low);
+      if (height[left] == low)
         ++left;
-      else
+      if (height[right] == low)
         --right;
     }
     return maxArea;
   }
-
 }
