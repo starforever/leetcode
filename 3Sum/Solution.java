1c6126409aab0a5634149098503a08a7fde66515
starforever <starforever00@gmail.com>
2014-01-05 21:04:27 +0800

    (3Sum) [Accepted] Code refinements

--- 3Sum/Solution.java
+++ 3Sum/Solution.java
@@ -1,13 +1,10 @@
 public class Solution
 {
 
-  int N;
-  int[] num;
-
-  HashMap<Integer, Integer> calcNumCount ()
+  HashMap<Integer, Integer> calcNumCount (int[] num)
   {
     HashMap<Integer, Integer> numCount = new HashMap<Integer, Integer>();
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < num.length; ++i)
     {
       if (!numCount.containsKey(num[i]))
         numCount.put(num[i], 1);
@@ -17,25 +14,31 @@ public class Solution
     return numCount;
   }
 
-  int next (int i)
+  int unique (int[] num)
   {
-    int j = i + 1;
-    while (j < N && num[j] == num[i])
-      ++j;
-    return j;
+    Arrays.sort(num);
+    int i = 0;
+    int n = 0;
+    while (i < num.length)
+    {
+      num[n++] = num[i];
+      int j = i + 1;
+      while (j < num.length && num[j] == num[i])
+        ++j;
+      i = j;
+    }
+    return n;
   }
 
   public ArrayList<ArrayList<Integer>> threeSum (int[] num)
   {
-    N = num.length;
-    this.num = num;
-    Arrays.sort(num);
-    HashMap<Integer, Integer> numCount = calcNumCount();
+    HashMap<Integer, Integer> numCount = calcNumCount(num);
+    int N = unique(num);
     ArrayList<ArrayList<Integer>> solList = new ArrayList<ArrayList<Integer>>();
-    for (int i = 0; i < N; i = next(i))
+    for (int i = 0; i < N; ++i)
     {
       int a = num[i];
-      for (int j = i + 1; j < N; j = next(j))
+      for (int j = i + ((numCount.get(a) == 1) ? 1 : 0); j < N; ++j)
       {
         int b = num[j];
         int c = -(a + b);
@@ -43,13 +46,7 @@ public class Solution
           continue;
         if (!numCount.containsKey(c))
           continue;
-        int leastCnt;
-        if (c > b)
-          leastCnt = 1;
-        else if (b > a)
-          leastCnt = 2;
-        else
-          leastCnt = 3;
+        int leastCnt = 1 + ((c == b) ? 1 : 0) + ((c == a) ? 1 : 0);
         if (numCount.get(c) >= leastCnt)
           solList.add(new ArrayList<Integer>(Arrays.asList(a, b, c)));
       }
