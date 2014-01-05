e520c18b3466ffa4975610f0ed5437536cb86fb0
starforever <starforever00@gmail.com>
2014-01-05 23:06:17 +0800

    (Swap Nodes in Pairs) [Accepted] Code refinements

--- Swap Nodes in Pairs/Solution.java
+++ Swap Nodes in Pairs/Solution.java
@@ -2,19 +2,20 @@ public class Solution
 {
   public ListNode swapPairs (ListNode head)
   {
-    ListNode cur = head, prev = null;
+    if (head == null || head.next == null)
+      return head;
+    ListNode cur = head.next.next, prev = head;
+    head.next.next = head;
+    head = head.next;
     while (cur != null && cur.next != null)
     {
       ListNode next = cur.next.next;
-      if (prev == null)
-        head = cur.next;
-      else
-        prev.next = cur.next;
+      prev.next = cur.next;
       cur.next.next = cur;
-      cur.next = next;
       prev = cur;
       cur = next;
     }
+    prev.next = cur;
     return head;
   }
 }
