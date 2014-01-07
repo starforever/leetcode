3f212eef0445f2ef74de00f474679b6f8b0f4ac5
starforever <starforever00@gmail.com>
2014-01-07 22:05:12 +0800

    (Length of Last Word) [Accepted] Code refinements

--- Length of Last Word/Solution.java
+++ Length of Last Word/Solution.java
@@ -2,13 +2,9 @@ public class Solution
 {
   public int lengthOfLastWord (String s)
   {
-    if (s == null)
-      return 0;
     int i = s.length() - 1;
     while (i >= 0 && s.charAt(i) == ' ')
       --i;
-    if (i < 0)
-      return 0;
     int len = 0;
     while (i >= 0 && s.charAt(i) != ' ')
     {
