a9c0b00203008263dd188a8e64d4fa2598dae928
starforever <starforever00@gmail.com>
2014-01-07 19:29:20 +0800

    (First Missing Positive) [Accepted] Code refinements

--- First Missing Positive/Solution.java
+++ First Missing Positive/Solution.java
@@ -1,25 +1,34 @@
 public class Solution
 {
+
+  int N;
+  int[] A;
+
+  void swap (int i, int j)
+  {
+    int t = A[i];
+    A[i] = A[j];
+    A[j] = t;
+  }
+
   public int firstMissingPositive (int[] A)
   {
-    if (A == null)
-      return 1;
-    int N = A.length;
+    this.A = A;
+    N = A.length;
     int i = 0;
     while (i < N)
     {
-      if (A[i] >= 1 && A[i] <= N && A[i] != i + 1 && A[A[i] - 1] != A[i])
-      {
-        int tmp = A[A[i] - 1];
-        A[A[i] - 1] = A[i];
-        A[i] = tmp;
-      }
+      if (A[i] >= 1 && A[i] <= N && A[A[i] - 1] != A[i])
+        swap(i, A[i] - 1);
       else
         ++i;
     }
     for (i = 0; i < N; ++i)
+    {
       if (A[i] != i + 1)
         return i + 1;
+    }
     return N + 1;
   }
+
 }
