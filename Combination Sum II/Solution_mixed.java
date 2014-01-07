b1806a832bccd35d5f67b8a6cfc9e904db4cf4f0
starforever <starforever00@gmail.com>
2014-01-07 18:51:20 +0800

    (Combination Sum II) [Accepted] Rewrite 'mixed' solution using DP for search pruning, result as slightly faster running time

--- Combination Sum II/Solution_mixed.java
+++ Combination Sum II/Solution_mixed.java
@@ -1,51 +1,79 @@
 public class Solution
 {
 
-  int C, T;
+  int T;
+
+  int N;
   int[] num;
+  int[] count;
 
   boolean[][] hasSol;
 
-  HashSet<ArrayList<Integer>> findSol (int c, int t)
+  ArrayList<ArrayList<Integer>> solList;
+  Stack<Integer> curSol;
+
+  void calcHasSol ()
   {
-    HashSet<ArrayList<Integer>> ret = new HashSet<ArrayList<Integer>>();
-    if (t < 0 || !hasSol[c][t])
-      return ret;
-    if (c == 0 && t == 0)
+    int C = num.length;
+    hasSol = new boolean[C + 1][T + 1];
+    hasSol[C][0] = true;
+    for (int t = 1; t <= T; ++t)
+      hasSol[C][t] = false;
+    for (int c = C - 1; c >= 0; --c)
     {
-      ret.add(new ArrayList<Integer>());
-      return ret;
+      for (int t = 0; t <= T; ++t)
+        hasSol[c][t] = hasSol[c + 1][t] || t >= num[c] && hasSol[c + 1][t - num[c]];
     }
-    for (ArrayList<Integer> sol : findSol(c - 1, t))
-      ret.add(sol);
-    for (ArrayList<Integer> sol : findSol(c - 1, t - num[c - 1]))
+  }
+
+  void uniqueCount ()
+  {
+    count = new int[num.length];
+    Arrays.fill(count, 0);
+    N = 0;
+    int i = 0;
+    while (i < num.length)
     {
-      sol.add(num[c - 1]);
-      ret.add(sol);
+      num[N] = num[i];
+      while (i < num.length && num[i] == num[N])
+      {
+        ++count[N];
+        ++i;
+      }
+      ++N;
     }
-    return ret;
+  }
+
+  void search (int idx, int idx0, int remain)
+  {
+    if (remain == 0)
+    {
+      solList.add(new ArrayList<Integer>(curSol));
+      return;
+    }
+    if (idx == N)
+      return;
+    if (!hasSol[idx0][remain])
+      return;
+    for (int n = 0; n <= count[idx] && num[idx] * n <= remain; ++n)
+    {
+      search(idx + 1, idx0 + count[idx], remain - num[idx] * n);
+      curSol.push(num[idx]);
+    }
+    while (!curSol.empty() && curSol.peek() == num[idx])
+      curSol.pop();
   }
 
   public ArrayList<ArrayList<Integer>> combinationSum2 (int[] num, int target)
   {
-    if (num == null)
-      num = new int[0];
+    this.T = target;
     this.num = num;
-    C = num.length;
-    T = target;
     Arrays.sort(num);
-    hasSol = new boolean[C + 1][T + 1];
-    hasSol[0][0] = true;
-    for (int t = 1; t <= T; ++t)
-      hasSol[0][t] = false;
-    for (int c = 1; c <= C; ++c)
-    {
-      for (int t = 0; t <= T; ++t)
-        hasSol[c][t] = hasSol[c - 1][t] || t >= num[c - 1] && hasSol[c - 1][t - num[c - 1]];
-    }
-    ArrayList<ArrayList<Integer>> solList = new ArrayList<ArrayList<Integer>>();
-    for (ArrayList<Integer> sol : findSol(C, T))
-      solList.add(sol);
+    calcHasSol();
+    uniqueCount();
+    solList = new ArrayList<ArrayList<Integer>>();
+    curSol = new Stack<Integer>();
+    search(0, 0, target);
     return solList;
   }
 
