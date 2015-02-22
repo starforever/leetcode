16ccc92fd01ad53225b31096414fd3a8db749f3b
starforever <starforever00@gmail.com>
2015-02-22 03:07:38 -0800

    (Best Time to Buy and Sell Stock IV) [Accepted] O(N^2) DP solution with quick solve and a lot of constant optimization

--- 188/Solution.java
+++ 188/Solution.java
@@ -2,42 +2,49 @@ public class Solution
 {
   public int maxProfit (int K, int[] prices)
   {
+    int sol = quickSolve(K, prices);
+    if (sol != -1)
+      return sol;
     int N = prices.length;
-    K = Math.min(K, numRise(prices));
-    int[][] gain = new int[N + 1][K + 1];
+    int[] gainCurrent = new int[N + 1];
+    int[] gainNext = new int[N + 1];
     for (int i = 0; i <= N; ++i)
-      gain[i][K] = 0;
+      gainCurrent[i] = 0;
     for (int k = K - 1; k >= 0; --k)
     {
-      gain[N - 1][k] = gain[N][k] = 0;
+      int[] tmp = gainCurrent;
+      gainCurrent = gainNext;
+      gainNext = tmp;
+
+      gainCurrent[N - 1] = gainCurrent[N] = 0;
       int opt = prices[N - 1];
       for (int i = N - 2; i >= 0; --i)
       {
-        gain[i][k] = gain[i + 1][k];
-        if (opt - prices[i] > gain[i][k])
-          gain[i][k] = opt - prices[i];
-        if (prices[i] + gain[i + 1][k + 1] > opt)
-          opt = prices[i] + gain[i + 1][k + 1];
+        gainCurrent[i] = Math.max(gainCurrent[i + 1], opt - prices[i]);
+        opt = Math.max(opt, prices[i] + gainNext[i + 1]);
       }
     }
-    return gain[0][0];
+    return gainCurrent[0];
   }
 
-  private int numRise (int[] prices)
+  private int quickSolve (int K, int[] prices)
   {
     int N = prices.length;
     int i = 0;
-    int cnt = 0;
+    int numRise = 0;
+    int totalGain = 0;
     while (i + 1 < N)
     {
       while (i + 1 < N && prices[i + 1] < prices[i])
         ++i;
       if (i + 1 >= N)
         break;
-      ++cnt;
+      ++numRise;
+      int j = i;
       while (i + 1 < N && prices[i + 1] >= prices[i])
         ++i;
+      totalGain += prices[i] - prices[j];
     }
-    return cnt;
+    return numRise <= K ? totalGain : -1;
   }
 }
