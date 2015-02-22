67541728c95d891049ab4156457be1c14232437b
starforever <starforever00@gmail.com>
2015-02-22 02:00:56 -0800

    (Best Time to Buy and Sell Stock IV) [Time Limit Exceeded] O(N^2) DP solution with constant optimization

--- 188/Solution.java
+++ 188/Solution.java
@@ -3,7 +3,7 @@ public class Solution
   public int maxProfit (int K, int[] prices)
   {
     int N = prices.length;
-    K = Math.min(K, N / 2);
+    K = Math.min(K, numRise(prices));
     int[][] gain = new int[N + 1][K + 1];
     for (int i = 0; i <= N; ++i)
       gain[i][K] = 0;
@@ -13,10 +13,31 @@ public class Solution
       int opt = prices[N - 1];
       for (int i = N - 2; i >= 0; --i)
       {
-        gain[i][k] = Math.max(gain[i + 1][k], opt - prices[i]);
-        opt = Math.max(opt, prices[i] + gain[i + 1][k + 1]);
+        gain[i][k] = gain[i + 1][k];
+        if (opt - prices[i] > gain[i][k])
+          gain[i][k] = opt - prices[i];
+        if (prices[i] + gain[i + 1][k + 1] > opt)
+          opt = prices[i] + gain[i + 1][k + 1];
       }
     }
     return gain[0][0];
+  }
+
+  private int numRise (int[] prices)
+  {
+    int N = prices.length;
+    int i = 0;
+    int cnt = 0;
+    while (i + 1 < N)
+    {
+      while (i + 1 < N && prices[i + 1] < prices[i])
+        ++i;
+      if (i + 1 >= N)
+        break;
+      ++cnt;
+      while (i + 1 < N && prices[i + 1] >= prices[i])
+        ++i;
+    }
+    return cnt;
   }
 }
