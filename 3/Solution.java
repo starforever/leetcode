ff7a667c46ed653513e89ebc6c4da9885c30cac1
starforever <starforever00@gmail.com>
2015-02-23 21:22:43 -0800

    (Longest Substring Without Repeating Characters) [Accepted] Support any ASCII codes in the string

--- 3/Solution.java
+++ 3/Solution.java
@@ -1,17 +1,21 @@
+import java.util.BitSet;
+
 public class Solution
 {
   public int lengthOfLongestSubstring (String s)
   {
-    int usedChar = 0;
+    if (s == null)
+      throw new IllegalArgumentException();
+    BitSet usedChar = new BitSet(128);
     int front = 0, back = 0;
-    while (front < s.length() && (usedChar & 1 << s.charAt(front) - 'a') == 0)
-      usedChar ^= 1 << s.charAt(front++) - 'a';
+    while (front < s.length() && !usedChar.get((int)s.charAt(front)))
+      usedChar.flip((int)s.charAt(front++));
     int maxLen = front;
     while (front < s.length())
     {
-      usedChar ^= 1 << s.charAt(back++) - 'a';
-      while (front < s.length() && (usedChar & 1 << s.charAt(front) - 'a') == 0)
-        usedChar ^= 1 << s.charAt(front++) - 'a';
+      usedChar.flip((int)s.charAt(back++));
+      while (front < s.length() && !usedChar.get((int)s.charAt(front)))
+        usedChar.flip((int)s.charAt(front++));
       maxLen = Math.max(maxLen, front - back);
     }
     return maxLen;
