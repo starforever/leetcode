f91b63ba3fe8f003134567812707a703a0fdf4ed
starforever <starforever00@gmail.com>
2015-12-11 00:52:57 -0800

    (Number of 1 Bits) [Accepted]

---
+++ 191/Solution.java
@@ -0,0 +1,14 @@
+public class Solution
+{
+  public int hammingWeight (int n)
+  {
+    int cnt = 0;
+    while (n != 0)
+    {
+      if ((n & 1) != 0)
+        ++cnt;
+      n >>>= 1;
+    }
+    return cnt;
+  }
+}
