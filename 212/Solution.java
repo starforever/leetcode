cf0ee18a02e5d01d1167188947ab58286e820635
starforever <starforever00@gmail.com>
2015-12-22 16:48:20 -0800

    (Word Search II) [Accepted] A simpler Trie implementation without HashMap. This leads to less memory consumption and faster query speed.

--- 212/Solution.java
+++ 212/Solution.java
@@ -1,7 +1,7 @@
 class TrieNode
 {
   int idx = -1;
-  HashMap<Character, TrieNode> next = new HashMap<Character, TrieNode>();
+  TrieNode[] next = new TrieNode[26];
 }
 
 public class Solution
@@ -23,9 +23,9 @@ public class Solution
       for (int j = 0; j < words[i].length(); ++j)
       {
         char c = words[i].charAt(j);
-        if (!p.next.containsKey(c))
-          p.next.put(c, new TrieNode());
-        p = p.next.get(c);
+        if (p.next[c - 'a'] == null)
+          p.next[c - 'a'] = new TrieNode();
+        p = p.next[c - 'a'];
       }
       p.idx = i;
     }
@@ -40,10 +40,10 @@ public class Solution
     for (int d = 0; d < 4; ++d)
     {
       int x2 = x + dx[d], y2 = y + dy[d];
-      if (x2 >= 0 && x2 < X && y2 >= 0 && y2 < Y && !vis[x2][y2] && p.next.containsKey(board[x2][y2]))
+      if (x2 >= 0 && x2 < X && y2 >= 0 && y2 < Y && !vis[x2][y2] && p.next[board[x2][y2] - 'a'] != null)
       {
         vis[x2][y2] = true;
-        search(x2, y2, p.next.get(board[x2][y2]));
+        search(x2, y2, p.next[board[x2][y2] - 'a']);
         vis[x2][y2] = false;
       }
     }
@@ -82,10 +82,10 @@ public class Solution
     for (int x = 0; x < X; ++x)
       for (int y = 0; y < Y; ++y)
       {
-        if (root.next.containsKey(board[x][y]))
+        if (root.next[board[x][y] - 'a'] != null)
         {
           vis[x][y] = true;
-          search(x, y, root.next.get(board[x][y]));
+          search(x, y, root.next[board[x][y] - 'a']);
           vis[x][y] = false;
         }
       }
